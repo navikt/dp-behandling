06ae4e93a44badc88c6123a2c77a062c9b1a188b
Geir André Lund <geir.andre.lund@nav.no>
2025-02-05 08:34:27 +0100

    Legger til kommentar for fremtidig referanse.

--- mediator/src/main/resources/db/migration/V45__ENDRE_PROVINGSDATO.sql
+++ mediator/src/main/resources/db/migration/V45__ENDRE_PROVINGSDATO.sql
@@ -2,6 +2,14 @@
 -- Siste exist query sjekker om det finnes en opplysning med samme opplysningstype_id som den nye opplysningen, det finnes behandlinger som allerede har _både_ ny og gammel versjon av opplysningen.
 
 --prod
+-- +------------------+-------------+------------+--------+------+---------------------------------+------+------------------------------------+
+-- |opplysningstype_id|behov_id     |navn        |datatype|parent|opprettet                        |formål|uuid                                |
+-- +------------------+-------------+------------+--------+------+---------------------------------+------+------------------------------------+
+-- |19216082          |Virkningsdato|Prøvingsdato|Dato    |null  |2024-10-21 08:47:35.475918 +00:00|Regel |e8cb6606-1e89-4733-b13d-b912ec4abf56|
+-- |39494007          |Prøvingsdato |Prøvingsdato|Dato    |null  |2025-01-16 12:39:22.695917 +00:00|Regel |0194881f-91d1-7df2-ba1d-4533f37fcc76|
+-- +------------------+-------------+------------+--------+------+---------------------------------+------+------------------------------------+
+
+
 UPDATE opplysning SET opplysningstype_id = 39494007 WHERE opplysningstype_id = 19216082 AND id IN (
     SELECT id FROM opplysning
                        JOIN opplysninger_opplysning oo ON opplysning.id = oo.opplysning_id
@@ -18,6 +26,13 @@ UPDATE opplysning SET opplysningstype_id = 39494007 WHERE opplysningstype_id = 1
     ));
 
 --dev
+-- +------------------+-------------+------------+--------+------+---------------------------------+------+------------------------------------+
+-- |opplysningstype_id|behov_id     |navn        |datatype|parent|opprettet                        |formål|uuid                                |
+-- +------------------+-------------+------------+--------+------+---------------------------------+------+------------------------------------+
+-- |636731            |Virkningsdato|Prøvingsdato|Dato    |null  |2024-10-21 10:45:06.628481 +00:00|Regel |b81a0b12-ec32-4135-b8e0-a7c60f84c759|
+-- |6886412           |Prøvingsdato |Prøvingsdato|Dato    |null  |2025-01-16 12:07:21.151743 +00:00|Regel |0194881f-91d1-7df2-ba1d-4533f37fcc76|
+-- +------------------+-------------+------------+--------+------+---------------------------------+------+------------------------------------+
+
 UPDATE opplysning SET opplysningstype_id = 6886412 WHERE opplysningstype_id = 636731 AND id IN (
     SELECT id FROM opplysning
                        JOIN opplysninger_opplysning oo ON opplysning.id = oo.opplysning_id
