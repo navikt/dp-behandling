4b33713c5c8c7eb323bf65298b13a319b8b359d1
Geir André Lund <geir.andre.lund@nav.no>
2025-02-10 10:18:46 +0100

    Openapi generator kotlin-server har ikke serializationLibrary overstyring og tvinger en over på kotlinx serialisering. Ser ikke ut fra release-loggen hvor den kommer fra.

--- openapi/build.gradle.kts
+++ openapi/build.gradle.kts
@@ -35,7 +35,7 @@ dependencies {
 }
 
 openApiGenerate {
-    generatorName.set("kotlin-server")
+    generatorName.set("kotlin")
     inputSpec.set("$projectDir/src/main/resources/behandling-api.yaml")
     outputDir.set("${layout.buildDirectory.get()}/generated/")
     packageName.set("no.nav.dagpenger.behandling.api")
