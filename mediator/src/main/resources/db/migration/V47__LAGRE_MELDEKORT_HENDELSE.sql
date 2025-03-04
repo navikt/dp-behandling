af0e2afa8c3bcb1fe5d510ef26caf5aa10dad083
Geir André Lund <geir.andre.lund@nav.no>
2025-03-04 12:03:53 +0100

    Meldekortid er unik.
    Co-authored-by: Eivind Havnelid Røyrvik <eivind.havnelid.royrvik@nav.no>
    Co-authored-by: Tonje Mjøvik Myran <tonje.mjovik.myran@nav.no>
    Co-authored-by: Morten Tholander <morten.tholander@nav.no>
    Co-authored-by: Richard André Martinsen <richard.martinsen@nav.no>
    Co-authored-by: André Roaldseth <andre.roaldseth@nav.no>

--- mediator/src/main/resources/db/migration/V47__LAGRE_MELDEKORT_HENDELSE.sql
+++ mediator/src/main/resources/db/migration/V47__LAGRE_MELDEKORT_HENDELSE.sql
@@ -2,7 +2,7 @@ CREATE TABLE meldekort_hendelse
 (
     id                     UUID PRIMARY KEY NOT NULL,
     løpenummer             BIGSERIAL        NOT NULL,
-    meldekort_id           BIGINT           NOT NULL,
+    meldekort_id           BIGINT           NOT NULL UNIQUE,
     korrigert_meldekort_id BIGINT REFERENCES meldekort_hendelse (meldekort_id),
     meldingsreferanse_id   UUID             NOT NULL REFERENCES melding (melding_id),
     ident                  TEXT             NOT NULL REFERENCES person (ident),
@@ -12,8 +12,6 @@ CREATE TABLE meldekort_hendelse
     kilde_rolle            TEXT             NOT NULL,
     kilde_ident            TEXT             NOT NULL,
     opprettet              TIMESTAMP        NOT NULL,
-    UNIQUE (meldekort_id)
-
 );
 
 CREATE INDEX meldekort_hendelse_ident_idx ON meldekort_hendelse (ident);
@@ -31,7 +29,7 @@ CREATE TABLE meldekort_aktivitet
 (
     meldekort_id BIGINT NOT NULL,
     dato         DATE   NOT NULL,
-    type         TEXT   NOT NULL, -- Arbeid, Fravaer, Syk, Utdanning
+    type         TEXT   NOT NULL, -- Arbeid, Fravær, Syk, Utdanning
     timer        INTERVAL,
     FOREIGN KEY (meldekort_id, dato) REFERENCES meldekort_dag (meldekort_id, dato)
 
