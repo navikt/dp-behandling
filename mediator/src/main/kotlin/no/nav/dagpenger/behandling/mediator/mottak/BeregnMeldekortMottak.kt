321ff8198d6759874728823f765b6b6c2f2f4f42
Geir André Lund <geir.andre.lund@nav.no>
2025-03-31 14:02:09 +0200

    Unskip skip fra Knappen™.
    Co-authored-by: Tonje Mjøvik Myran <tonje.mjovik.myran@nav.no>
    Co-authored-by: Eivind Havnelid Røyrvik <eivind.havnelid.royrvik@nav.no>
    Co-authored-by: Morten Tholander <morten.tholander@nav.no>
    Co-authored-by: Udo Huschens <udo.huschens@nav.no>
    Co-authored-by: Eivind Havnelid Røyrvik <eivind.havnelid.royrvik@nav.no>
    Co-authored-by: Richard André Martinsen <richard.martinsen@nav.no>

--- mediator/src/main/kotlin/no/nav/dagpenger/behandling/mediator/mottak/BeregnMeldekortMottak.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/behandling/mediator/mottak/BeregnMeldekortMottak.kt
@@ -30,20 +30,7 @@ internal class BeregnMeldekortMottak(
             }.register(this)
     }
 
-    private val skipMeldekort =
-        setOf<String>(
-            "0195eb1b-0e02-7d0e-abda-97d2cf6b791d",
-            "01956154-41c7-7c00-aa90-e1f1b8b9cd0f",
-            "01957044-adad-7e3a-9ad2-41cc0b0b6266",
-            "01957ede-3ff5-7e41-9b49-dcbdb8e4be8b",
-            "01957fcf-ec3a-7c95-aa16-57f41bdad467",
-            "0195801f-0a5c-7475-a181-13ee1ce3b4e6",
-            "01958025-9040-71f1-a318-d244433592ae",
-            "01958a64-b67d-759e-b7ce-ad7830f88998",
-            "01958e85-75e9-7d8d-a798-f3fea02f4f57",
-            "0195a3b9-4eab-7ef8-b27a-fe25221237e3",
-            "0195a8c6-f175-7d9f-a869-d176daf98175",
-        )
+    private val skipMeldekort = emptySet<String>()
 
     @WithSpan
     override fun onPacket(
