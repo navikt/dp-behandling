a3923755489a9afa37c62941a60840d1e8dff9b7
Geir André Lund <geir.andre.lund@nav.no>
2025-05-19 15:30:26 +0200

    De-skip behandlinger i opplysningsvarmottak

--- mediator/src/main/kotlin/no/nav/dagpenger/behandling/mediator/mottak/OpplysningSvarMottak.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/behandling/mediator/mottak/OpplysningSvarMottak.kt
@@ -67,13 +67,10 @@ internal class OpplysningSvarMottak(
             }.register(this)
     }
 
-    private val skipBehovId = listOf("f034d0cc-729e-446f-9177-fc6566f8d20a")
+    private val skipBehovId = listOf("ingen-treff")
     private val skipBehandlingsId =
         listOf(
-            "0196cb39-3928-731a-b670-2ceefe03ac27",
-            "0196cb32-7a1d-71be-8e27-7ca790414c6c",
-            "0196ce9c-0982-7e50-8467-e83d33c53146",
-            "0196cea1-056c-7e39-91b1-4fe85e22c0db",
+            "ingen-treff",
         )
 
     @WithSpan
