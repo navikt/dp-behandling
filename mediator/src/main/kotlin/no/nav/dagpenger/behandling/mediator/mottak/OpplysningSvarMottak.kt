e87e1922e55ff102f25771897feb1b498bbeb5bd
Geir André Lund <geir.andre.lund@nav.no>
2025-05-19 14:05:19 +0200

    Skipper behandling

--- mediator/src/main/kotlin/no/nav/dagpenger/behandling/mediator/mottak/OpplysningSvarMottak.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/behandling/mediator/mottak/OpplysningSvarMottak.kt
@@ -68,7 +68,8 @@ internal class OpplysningSvarMottak(
     }
 
     private val skipBehovId = listOf("f034d0cc-729e-446f-9177-fc6566f8d20a")
-    private val skipBehandlingsId = listOf("0196cb39-3928-731a-b670-2ceefe03ac27", "0196cb32-7a1d-71be-8e27-7ca790414c6c")
+    private val skipBehandlingsId =
+        listOf("0196cb39-3928-731a-b670-2ceefe03ac27", "0196cb32-7a1d-71be-8e27-7ca790414c6c", "0196ce9c-0982-7e50-8467-e83d33c53146")
 
     @WithSpan
     override fun onPacket(
