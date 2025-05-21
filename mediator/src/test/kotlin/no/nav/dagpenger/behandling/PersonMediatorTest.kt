897ce5a23bd1b46805982b7695ce50015e6e69e5
Geir André Lund <geir.andre.lund@nav.no>
2025-05-21 09:56:46 +0200

    Endre tilbake navn på vilkår som går ut av dp-behandling.
    Co-authored-by: Tonje Mjøvik Myran <tonje.mjovik.myran@nav.no>

--- mediator/src/test/kotlin/no/nav/dagpenger/behandling/PersonMediatorTest.kt
+++ mediator/src/test/kotlin/no/nav/dagpenger/behandling/PersonMediatorTest.kt
@@ -282,7 +282,7 @@ internal class PersonMediatorTest {
                 medVilkår("Oppfyller kravet til minsteinntekt") {
                     erIkkeOppfylt()
                 }
-                medVilkår("Reell arbeidssøker") {
+                medVilkår("Krav til arbeidssøker") {
                     erOppfylt()
                 }
                 /*
