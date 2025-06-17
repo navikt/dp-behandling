4d5cae05714a5eb763e3c5197b00b0243c218d8a
André Roaldseth <andre.roaldseth@nav.no>
2025-06-17 14:35:36 +0200

    Skip behov som feiler
    Co-authored-by: Richard Andre Martinsen <richard.martinsen@nav.no>
    Co-authored-by: Morten Tholander <morten.tholander@nav.no>

--- mediator/src/main/kotlin/no/nav/dagpenger/behandling/mediator/mottak/OpplysningSvarMottak.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/behandling/mediator/mottak/OpplysningSvarMottak.kt
@@ -67,7 +67,7 @@ internal class OpplysningSvarMottak(
             }.register(this)
     }
 
-    private val skipBehovId = listOf("f162f0a2-7c07-42cf-982e-b98281340339")
+    private val skipBehovId = listOf("efc7112b-8f76-4ee9-8c3e-670ce47cc5d2")
     private val skipBehandlingsId = listOf("0196f70d-baf3-708a-8741-6e9c5e03448a")
 
     @WithSpan
