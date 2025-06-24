dc034b2fdfc177322148f3ddf15ee693c48001ae
Geir André Lund <geir.andre.lund@nav.no>
2025-06-24 14:16:05 +0200

    Skipper bad meldekort.

--- mediator/src/main/kotlin/no/nav/dagpenger/behandling/mediator/mottak/BeregnMeldekortMottak.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/behandling/mediator/mottak/BeregnMeldekortMottak.kt
@@ -30,7 +30,7 @@ internal class BeregnMeldekortMottak(
             }.register(this)
     }
 
-    private val skipMeldekort = setOf("ingen")
+    private val skipMeldekort = setOf("0197a18e-8109-7f8d-b17a-05f29168b974")
 
     @WithSpan
     override fun onPacket(
