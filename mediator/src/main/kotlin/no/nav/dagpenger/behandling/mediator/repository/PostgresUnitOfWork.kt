76d64d25284546f6ac32368187e5cdde6a82a241
André Roaldseth <andre.roaldseth@nav.no>
2025-07-11 14:34:40 +0200

    Reduser sannsynligheten for connection leaks
    Co-authored-by: Tonje Mjøvik Myran <tonje.mjovik.myran@nav.no>

--- mediator/src/main/kotlin/no/nav/dagpenger/behandling/mediator/repository/PostgresUnitOfWork.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/behandling/mediator/repository/PostgresUnitOfWork.kt
@@ -4,23 +4,32 @@ import kotliquery.Session
 import kotliquery.sessionOf
 import no.nav.dagpenger.behandling.db.PostgresDataSourceBuilder.dataSource
 
-class PostgresUnitOfWork private constructor(private val session: Session) : UnitOfWork<Session> {
+class PostgresUnitOfWork private constructor(
+    private val session: Session,
+) : UnitOfWork<Session> {
     companion object {
         fun transaction() = PostgresUnitOfWork(sessionOf(dataSource)).apply { begin() }
     }
 
     private fun begin() = session.connection.begin()
 
-    override fun commit() = session.use { it.connection.commit() }
+    override fun commit() {
+        session.connection.commit()
+        session.close()
+    }
 
-    override fun rollback() = session.use { it.connection.rollback() }
+    override fun rollback() {
+        session.connection.rollback()
+        session.close()
+    }
 
-    override fun <T> inTransaction(block: (Session) -> T) =
+    override fun <T> inTransaction(block: (Session) -> T): T =
         try {
             block(session)
         } catch (e: Exception) {
             // TODO Rollback kan ikke gjøres fordi connection er allerede stengt og maskerer feil mot PostgreSQL
             // rollback()
+            session.close()
             throw e
         }
 }
