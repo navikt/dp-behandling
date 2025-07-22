e74879ec374b9955de237a115653777330ef3281
André Roaldseth <andre.roaldseth@nav.no>
2025-07-22 12:35:05 +0200

    Skru på Hikari sin pool connection til default-verdier
    Verdiene som lå der bærer veldig preg av on-prem postgres hvor det var veldig trangt om plassen for connections
    
    Co-authored-by: Geir André Lund <geir.andre.lund@nav.no>
    Co-authored-by: Eivind Havnelid Røyrvik <eivind.havnelid.royrvik@nav.no>

--- mediator/src/main/kotlin/no/nav/dagpenger/behandling/db/PostgresDataSourceBuilder.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/behandling/db/PostgresDataSourceBuilder.kt
@@ -6,6 +6,7 @@ import com.zaxxer.hikari.HikariDataSource
 import org.flywaydb.core.Flyway
 import org.flywaydb.core.api.configuration.FluentConfiguration
 import org.flywaydb.core.internal.configuration.ConfigUtils
+import kotlin.time.Duration.Companion.minutes
 import kotlin.time.Duration.Companion.seconds
 
 // Understands how to create a data source from environment variables
@@ -21,13 +22,17 @@ internal object PostgresDataSourceBuilder {
             jdbcUrl = getOrThrow(DB_URL_KEY).ensurePrefix("jdbc:postgresql://").stripCredentials()
             username = getOrThrow(DB_USERNAME_KEY)
             password = getOrThrow(DB_PASSWORD_KEY)
+            // Default 10
             maximumPoolSize = 10
-            minimumIdle = 1
-            idleTimeout = 10001
-            connectionTimeout = 1000
+            // Default 30 sekund
+            connectionTimeout = 10.seconds.inWholeMilliseconds
+            // Default 10 minutter
+            idleTimeout = 10.minutes.inWholeMilliseconds
+            // Default 2 minutter
+            keepaliveTime = 2.minutes.inWholeMilliseconds
+            // Default 30 minutter
+            maxLifetime = 30.minutes.inWholeMilliseconds
             leakDetectionThreshold = 2.seconds.inWholeMilliseconds
-            maxLifetime = 30001
-            initializationFailTimeout = 5000
         }
     }
 
