1a40f8005daf54141f0b881d2881bb14e3ddeec6
André Roaldseth <andre.roaldseth@nav.no>
2025-07-23 09:53:31 +0200

    Legg til en indeks som hjelper vaktmesteren i å finne opplysninger som har blitt fjernet

--- mediator/src/test/kotlin/no/nav/dagpenger/behandling/db/PostgresMigrationTest.kt
+++ mediator/src/test/kotlin/no/nav/dagpenger/behandling/db/PostgresMigrationTest.kt
@@ -10,7 +10,7 @@ class PostgresMigrationTest {
     fun `Migration scripts are applied successfully`() {
         withCleanDb {
             val migrations = runMigration()
-            migrations shouldBeExactly 57
+            migrations shouldBeExactly 58
         }
     }
 }
