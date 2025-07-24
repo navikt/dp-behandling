ace3c84a38f63c2f6ec84a748d553067cc9cb000
André Roaldseth <andre.roaldseth@nav.no>
2025-07-24 12:43:23 +0200

    Lag en logikk for å håndtere kjeding av ny behandling
    Co-authored-by: Geir André Lund <geir.andre.lund@nav.no>
    Co-authored-by: Eivind Havnelid Røyrvik <eivind.havnelid.royrvik@nav.no>

---
+++ mediator/src/test/kotlin/no/nav/dagpenger/behandling/KjedescenarioTest.kt
@@ -0,0 +1,104 @@
+package no.nav.dagpenger.behandling
+
+import io.kotest.matchers.collections.shouldBeEmpty
+import io.kotest.matchers.collections.shouldContainExactly
+import io.kotest.matchers.shouldBe
+import no.nav.dagpenger.behandling.scenario.SimulertDagpengerSystem.Companion.nyttScenario
+import org.junit.jupiter.api.Test
+
+// Tester ulike scenarier for kjeding av behandlinger
+class KjedescenarioTest {
+    @Test
+    fun `flere søknader på eksisterende kjede skal opprettes i parallell`() {
+        /*
+         * Når bruker har en behandlingskjede med vedtak om innvilgelse, og søker på nytt
+         * skal det opprettes en ny behandling for hver søknad som kjedes på siste ferdige.
+         *
+         * Bare en (parallell) behandling kan bli ferdigstilt.
+         * De andre parallelle behandlinge må avbrytes.
+         * Saksbehandler må manuelt avbryte behandlingene som ikke skal behandles.
+         */
+        nyttScenario {
+            inntektSiste12Mnd = 500000
+        }.test {
+            person.søkDagpenger(21.juni(2018))
+
+            behovsløsere.løsTilForslag()
+            saksbehandler.lukkAlleAvklaringer()
+            saksbehandler.godkjenn()
+            saksbehandler.beslutt()
+
+            vedtak {
+                utfall shouldBe true
+            }
+
+            val sisteFerdige = person.behandlingId
+
+            // Personen søker tre ganger før forrige søknad er behandlet
+            repeat(3) {
+                person.søkDagpenger(21.august(2018))
+                behovsløsere.løsTilForslag()
+                person.behandling.basertPåBehandlinger.shouldContainExactly(sisteFerdige)
+            }
+        }
+    }
+
+    @Test
+    fun `flere søknader skal ikke kjedes når siste behandling var avslag`() {
+        /*
+         * Når bruker har en behandlingskjede med vedtak om avslag, og søker på nytt
+         * skal det opprettes en ny behandling for hver søknad som ikke kjedes på siste ferdige.
+         *
+         * Saksbehandler må manuelt avbryte behandlingene som ikke skal behandles.
+         */
+        nyttScenario {
+            inntektSiste12Mnd = 5000
+        }.test {
+            person.søkDagpenger(21.juni(2018))
+
+            behovsløsere.løsTilForslag()
+            saksbehandler.lukkAlleAvklaringer()
+            saksbehandler.godkjenn()
+
+            vedtak {
+                utfall shouldBe false
+            }
+
+            // Personen søker tre ganger før forrige søknad er behandlet
+            repeat(3) {
+                person.søkDagpenger(21.august(2018))
+                behovsløsere.løsTilForslag()
+                person.behandling.basertPåBehandlinger.shouldBeEmpty()
+            }
+        }
+    }
+
+    @Test
+    fun `bruker kan ikke gjenoppta fordi forrige periode er for lenge siden`() {
+        /*
+         * Når bruker søker og kan ikke gjenoppta fordi forrige periode er for lenge siden.
+         *
+         * Behandlingen blir kjedet på forrige ferdige behandling.
+         * Saksbehandler må manuelt flytte behandlingen til en ny kjede.
+         */
+        nyttScenario {
+            inntektSiste12Mnd = 500000
+        }.test {
+            person.søkDagpenger(21.juni(2015))
+
+            behovsløsere.løsTilForslag()
+            saksbehandler.lukkAlleAvklaringer()
+            saksbehandler.godkjenn()
+            saksbehandler.beslutt()
+
+            vedtak {
+                utfall shouldBe true
+            }
+
+            person.søkDagpenger(21.august(2020))
+
+            // TODO: Fiks dette
+            // saksbehandler.flyttBehandlingTilNyKjede()
+        }
+    }
+}
