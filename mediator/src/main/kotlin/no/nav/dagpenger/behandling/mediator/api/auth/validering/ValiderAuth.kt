7bf978ce281c389bedda3a2c21d783c0275d0b24
Geir André Lund <geir.andre.lund@nav.no>
2025-08-01 13:59:35 +0200

    typo

--- mediator/src/main/kotlin/no/nav/dagpenger/behandling/mediator/api/auth/validering/ValiderAuth.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/behandling/mediator/api/auth/validering/ValiderAuth.kt
@@ -15,7 +15,7 @@ internal fun JWTAuthenticationProvider.Config.autoriser() {
 
     validate { jwtClaims: JWTCredential ->
         val type = jwtClaims.payload.claims["idtyp"]?.asString()
-        logger.info("Tilgangsjekker idtype: $type")
+        logger.info("Tilgangsjekker idtyp: $type")
         when (type) {
             "app" -> {
                 jwtClaims.tilgangsjekkForMaskinToken(apper)
