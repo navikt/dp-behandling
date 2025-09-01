98a3ae1246fb02aa2097594fbf3b400a637b98cc
André Roaldseth <andre.roaldseth@nav.no>
2025-09-01 14:14:32 +0200

    Hopp over behov som stanger
    Co-authored-by: Tonje Mjøvik Myran <tonje.mjovik.myran@nav.no>
    Co-authored-by: Geir André Lund <geir.andre.lund@nav.no>
    Co-authored-by: Richard Andre Martinsen <richard.martinsen@nav.no>

--- mediator/src/main/kotlin/no/nav/dagpenger/behandling/mediator/mottak/OpplysningSvarMottak.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/behandling/mediator/mottak/OpplysningSvarMottak.kt
@@ -69,7 +69,7 @@ internal class OpplysningSvarMottak(
             }.register(this)
     }
 
-    private val skipBehovId = listOf("8d2ca48c-d68f-4a06-880c-833857b28c55")
+    private val skipBehovId = listOf("019c73ab-bd40-48ae-88c8-5be283de785e")
     private val skipBehandlingsId = listOf("01987502-cbbf-7fd1-9890-0e35101ba008")
 
     @WithSpan
