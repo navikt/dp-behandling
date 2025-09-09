e32cdefdeb4d2e97d9dbec164621c39ea07fe067
Geir André Lund <geir.andre.lund@nav.no>
2025-09-09 09:07:11 +0200

    Skipper broken opplysningsvarbehov.

--- mediator/src/main/kotlin/no/nav/dagpenger/behandling/mediator/mottak/OpplysningSvarMottak.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/behandling/mediator/mottak/OpplysningSvarMottak.kt
@@ -69,7 +69,7 @@ internal class OpplysningSvarMottak(
             }.register(this)
     }
 
-    private val skipBehovId = listOf("8d2ca48c-d68f-4a06-880c-833857b28c55")
+    private val skipBehovId = listOf("242cdbcb-0680-4e51-b6d8-1605edebe1c8")
     private val skipBehandlingsId = listOf("01987502-cbbf-7fd1-9890-0e35101ba008")
 
     @WithSpan
