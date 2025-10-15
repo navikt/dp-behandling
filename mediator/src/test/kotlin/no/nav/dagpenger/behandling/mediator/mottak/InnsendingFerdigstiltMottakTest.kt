7ebe05f2da0c86f583a5d3e5a650ed4def34aeee
Geir André Lund <geir.andre.lund@nav.no>
2025-10-15 10:41:26 +0200

    WIP - Ta i mot gjenopptak søknader som ikke har fagsakId

--- mediator/src/test/kotlin/no/nav/dagpenger/behandling/mediator/mottak/InnsendingFerdigstiltMottakTest.kt
+++ mediator/src/test/kotlin/no/nav/dagpenger/behandling/mediator/mottak/InnsendingFerdigstiltMottakTest.kt
@@ -1,18 +1,18 @@
 package no.nav.dagpenger.behandling.mediator.mottak
 
+import com.github.navikt.tbd_libs.rapids_and_rivers.JsonMessage
 import com.github.navikt.tbd_libs.rapids_and_rivers.test_support.TestRapid
 import com.github.navikt.tbd_libs.rapids_and_rivers_api.MessageContext
 import io.kotest.matchers.shouldBe
-import io.mockk.every
 import io.mockk.mockk
+import io.mockk.verify
 import no.nav.dagpenger.behandling.mediator.MessageMediator
 import no.nav.dagpenger.behandling.mediator.asUUID
 import no.nav.dagpenger.behandling.modell.hendelser.StartHendelse
-import org.intellij.lang.annotations.Language
 import org.junit.jupiter.api.Test
 import java.util.UUID
 
-class InnsendingFerdigstiltMottakTest {
+internal class InnsendingFerdigstiltMottakTest {
     private val messageMediator = mockk<MessageMediator>(relaxed = true)
 
     private val rapid =
@@ -25,10 +25,11 @@ class InnsendingFerdigstiltMottakTest {
 
     @Test
     fun `tar imot søknad om ny og republiserer som behandlingsklar`() {
-        rapid.sendTestMessage(innsendingJSON())
+        rapid.sendTestMessage(søknad())
         rapid.inspektør.size shouldBe 1
         rapid.inspektør.key(0) shouldBe ident
-        with(rapid.inspektør.message(0)) {
+        val message = rapid.inspektør.message(0)
+        with(message) {
             this["ident"].asText() shouldBe ident
             this["fagsakId"].asInt() shouldBe 123
             this["journalpostId"].asInt() shouldBe 123
@@ -36,7 +37,9 @@ class InnsendingFerdigstiltMottakTest {
             this["type"].asText() shouldBe "NySøknad"
         }
 
-        every {
+        rapid.sendTestMessage(message.toString())
+
+        verify {
             messageMediator.behandle(
                 any<StartHendelse>(),
                 any<SøknadInnsendtMessage>(),
@@ -47,19 +50,21 @@ class InnsendingFerdigstiltMottakTest {
 
     @Test
     fun `tar imot søknad om gjenopptak og republiserer som behandlingsklar`() {
-        rapid.sendTestMessage(innsendingJSON("Gjenopptak"))
+        rapid.sendTestMessage(søknad(type = "Gjenopptak", fagsakId = null))
         rapid.inspektør.size shouldBe 1
         rapid.inspektør.key(0) shouldBe ident
 
-        with(rapid.inspektør.message(0)) {
+        val message = rapid.inspektør.message(0)
+        with(message) {
             this["ident"].asText() shouldBe ident
-            this["fagsakId"].asInt() shouldBe 123
+            this["fagsakId"].asInt() shouldBe 0
             this["journalpostId"].asInt() shouldBe 123
             this["søknadId"].asUUID() shouldBe søknadId
             this["type"].asText() shouldBe "Gjenopptak"
         }
 
-        every {
+        rapid.sendTestMessage(message.toString())
+        verify {
             messageMediator.behandle(
                 any<StartHendelse>(),
                 any<SøknadInnsendtMessage>(),
@@ -70,22 +75,18 @@ class InnsendingFerdigstiltMottakTest {
 
     @Test
     fun `tar ikke imot innsendinger vi ikke vet om`() {
-        rapid.sendTestMessage(innsendingJSON("Utdanning"))
+        rapid.sendTestMessage(søknad("Utdanning"))
         rapid.inspektør.size shouldBe 0
     }
 
-    @Language("JSON")
-    fun innsendingJSON(type: String = "NySøknad") =
-        """
-        {
-          "@event_name": "innsending_ferdigstilt",
-          "type": "$type",
-          "fødselsnummer": "$ident",
-          "fagsakId": "123",
-          "søknadsData": {
-            "søknad_uuid": "$søknadId"
-          },
-          "journalpostId": "123"
-        }
-        """.trimIndent()
+    fun søknad(
+        type: String = "NySøknad",
+        fagsakId: Int? = 123,
+    ) = buildMap {
+        put("type", type)
+        if (fagsakId != null) put("fagsakId", fagsakId)
+        put("fødselsnummer", ident)
+        put("søknadsData", mapOf("søknad_uuid" to søknadId))
+        put("journalpostId", "123")
+    }.let { JsonMessage.newMessage("innsending_ferdigstilt", it).toJson() }
 }
