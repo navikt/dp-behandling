3ba76b9404fc3922ada0ea29370e6c2b91fa15c5
André Roaldseth <andre.roaldseth@nav.no>
2025-10-22 08:25:21 +0200

    Logg hvilke params vi har når inserts av opplysning kræsjer

--- mediator/src/main/kotlin/no/nav/dagpenger/behandling/mediator/repository/BatchStatement.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/behandling/mediator/repository/BatchStatement.kt
@@ -1,7 +1,19 @@
 package no.nav.dagpenger.behandling.mediator.repository
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import kotliquery.Session
 
-internal data class BatchStatement(private val query: String, private val params: List<Map<String, Any?>>) {
-    fun run(tx: Session) = tx.batchPreparedNamedStatement(query, params)
+private val sikkerlogg = KotlinLogging.logger("tjenestekall.BatchStatement")
+
+internal data class BatchStatement(
+    private val query: String,
+    private val params: List<Map<String, Any?>>,
+) {
+    fun run(tx: Session) =
+        try {
+            tx.batchPreparedNamedStatement(query, params)
+        } catch (e: Exception) {
+            sikkerlogg.error(e) { "Feil ved kjøring av batch statement: $query med params: $params" }
+            throw e
+        }
 }
