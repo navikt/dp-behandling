6e8b515867f1338a2e132887c62c422f21077ebb
André Roaldseth <andre.roaldseth@nav.no>
2025-10-22 08:38:03 +0200

    Revert "Logg hvilke params vi har når inserts av opplysning kræsjer"
    This reverts commit 3ba76b9404fc3922ada0ea29370e6c2b91fa15c5.

--- mediator/src/main/kotlin/no/nav/dagpenger/behandling/mediator/repository/BatchStatement.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/behandling/mediator/repository/BatchStatement.kt
@@ -1,19 +1,7 @@
 package no.nav.dagpenger.behandling.mediator.repository
 
-import io.github.oshai.kotlinlogging.KotlinLogging
 import kotliquery.Session
 
-private val sikkerlogg = KotlinLogging.logger("tjenestekall.BatchStatement")
-
-internal data class BatchStatement(
-    private val query: String,
-    private val params: List<Map<String, Any?>>,
-) {
-    fun run(tx: Session) =
-        try {
-            tx.batchPreparedNamedStatement(query, params)
-        } catch (e: Exception) {
-            sikkerlogg.error(e) { "Feil ved kjøring av batch statement: $query med params: $params" }
-            throw e
-        }
+internal data class BatchStatement(private val query: String, private val params: List<Map<String, Any?>>) {
+    fun run(tx: Session) = tx.batchPreparedNamedStatement(query, params)
 }
