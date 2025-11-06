96c7a132d71a372bfa8f49f34d3cd13e5f14fb12
Geir André Lund <geir.andre.lund@nav.no>
2025-11-06 14:01:56 +0100

    Sjekk om det streng "null"
    Co-authored-by: Richard André Martinsen <richard.martinsen@nav.no>

--- mediator/src/main/kotlin/no/nav/dagpenger/behandling/mediator/OpplysningSvarBygger.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/behandling/mediator/OpplysningSvarBygger.kt
@@ -60,7 +60,7 @@ fun barnMapper(
             )
         BarnetilleggV2 -> {
             BarnListe(
-                søknadbarnId = verdi["søknadbarnId"]?.asText()?.takeIf { it.isNotBlank() }?.let { UUID.fromString(it) },
+                søknadbarnId = verdi["søknadbarnId"]?.asText()?.takeIf { it.isNotBlank() || it == "null" }?.let { UUID.fromString(it) },
                 barn =
                     verdi["barn"].map {
                         Barn(
