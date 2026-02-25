a174c02d8b161bebad06723042436d9c88b7d9d5
Geir André Lund <geir.andre.lund@nav.no>
2026-02-25 13:43:05 +0100

    Legger "Meldt i tide" som redigerbar.
    Co-authored-by: Morten Tholander <morten.tholander@nav.no>
    Co-authored-by: Udo Huschens <udo.huschens@nav.no>
    Co-authored-by: Tatsiana Hrybko <tatsiana.hrybko@nav.no>
    Co-authored-by: Richard André Martinsen <richard.martinsen@nav.no>

--- mediator/src/main/kotlin/no/nav/dagpenger/behandling/mediator/api/BehandlingApiMapper.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/behandling/mediator/api/BehandlingApiMapper.kt
@@ -313,6 +313,7 @@ internal val redigerbareOpplysninger =
 
                     // MANUELL overstyring av beregning
                     if (unleash.isEnabled(Feature.REDIGERING_AV_BEREGNING.navn)) {
+                        add(Beregning.meldtITide)
                         add(Beregning.forbruk)
                         add(Beregning.forbruktEgenandel)
                         add(Beregning.gjenståendeEgenandel)
