02516eecd94fd714aab91dbb1dbd03b945926ae1
Geir André Lund <geir.andre.lund@nav.no>
2026-03-26 13:33:29 +0100

    Skipper behandling som spør om registrert som arbeidssøker om og om igjen.

--- mediator/src/main/kotlin/no/nav/dagpenger/behandling/mediator/mottak/OpplysningSvarMottak.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/behandling/mediator/mottak/OpplysningSvarMottak.kt
@@ -72,7 +72,7 @@ internal class OpplysningSvarMottak(
     private val skipBehovId = emptyList<String>()
     private val skipBehandlingsId =
         listOf(
-            "019d1a74-392c-7ad5-8def-484c63b585c2",
+            "019d1a74-5edb-7810-85c6-e15c17bc3893",
         )
 
     @WithSpan
