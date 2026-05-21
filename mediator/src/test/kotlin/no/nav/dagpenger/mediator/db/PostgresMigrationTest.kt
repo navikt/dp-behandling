f79993fa7eef9e27f43336fc60f0e7f58511ff2f
david steinsland <david@davidsteinsland.net>
2026-05-21 13:15:32 +0200

    migrerer én gang - tømmer tabeller etter hver test

--- mediator/src/test/kotlin/no/nav/dagpenger/mediator/db/PostgresMigrationTest.kt
+++ mediator/src/test/kotlin/no/nav/dagpenger/mediator/db/PostgresMigrationTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test
 class PostgresMigrationTest {
     @Test
     fun `Migration scripts are applied successfully`() {
-        withCleanDb {
+        withMigratedDb {
             val migrations = runMigration()
             migrations shouldBeExactly 25
         }
