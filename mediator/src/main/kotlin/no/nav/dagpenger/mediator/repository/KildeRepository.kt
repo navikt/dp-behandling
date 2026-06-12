bc6facd2ef6c59d0b333b5d231965c2828bcc875
André Roaldseth <andre.roaldseth@nav.no>
2026-06-12 19:06:37 +0200

    Deserialiser Behovsløserkilde til Behovsløserkilde

--- mediator/src/main/kotlin/no/nav/dagpenger/mediator/repository/KildeRepository.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/mediator/repository/KildeRepository.kt
@@ -66,7 +66,7 @@ internal class KildeRepository(
                         }
 
                         Behovsløserkilde::class.java.simpleName -> {
-                            Systemkilde(
+                            Behovsløserkilde(
                                 row.uuid("system_melding_id"),
                                 opprettet,
                                 kildeId,
