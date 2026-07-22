e28e67b0fb6832b24cf447fe90b3bd9b5529370f
Geir André Lund <geir.andre.lund@nav.no>
2026-07-22 10:17:18 +0200

    Revert "Etter endring i commit 91413dae4644a5e4bd16e5f523dce95ebdd1fc72 må vi kunne revurdere meldeplikten ved gjennopptak."
    This reverts commit 39e55860117963c24fa851c0412f4016f5dfcc62.

--- dagpenger/src/main/kotlin/no/nav/dagpenger/regel/regelsett/vilkår/Meldeplikt.kt
+++ dagpenger/src/main/kotlin/no/nav/dagpenger/regel/regelsett/vilkår/Meldeplikt.kt
@@ -7,14 +7,13 @@ import no.nav.dagpenger.opplysning.regel.somUtgangspunkt
 import no.nav.dagpenger.regel.OpplysningsTyper.OppfyllerMeldepliktId
 import no.nav.dagpenger.regel.regelsett.vilkår.KravPåDagpenger.harLøpendeRett
 import no.nav.dagpenger.regel.regelsett.vilkår.Rettighetstype.skalEksportVurderes
-import no.nav.dagpenger.regel.regelsett.vilkår.Rettighetstype.skalVernepliktVurderes
 
 object Meldeplikt {
     val oppfyllerMeldeplikt = boolsk(OppfyllerMeldepliktId, "Oppfyller meldeplikt")
 
     val regelsett =
         vilkår(folketrygden.hjemmel(4, 8, "Meldeplikt og møteplikt", "Meldeplikt")) {
-            skalVurderes { it.erSann(harLøpendeRett) || it.erSann(skalVernepliktVurderes) }
+            skalVurderes { it.erSann(harLøpendeRett) }
 
             utfall(oppfyllerMeldeplikt) { somUtgangspunkt(true, Søknadstidspunkt.søknadsdato) }
 
